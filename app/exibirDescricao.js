ddd760f937804313214f1e21e60d1f6d9337a4f0
Marcio Jose Martins Camara <118389170+marciomartins12@users.noreply.github.com>
2023-07-29 04:35:03 -0300

    Add files via upload

--- app/exibirDescricao.js
+++ app/exibirDescricao.js
@@ -5,13 +5,15 @@ function inputExibir(){
 elemento.addEventListener("change", (elemento)=>{
     const campoFixed = elemento.target.parentElement.parentElement.parentElement
     const descricaoParagrafoItem = elemento.target.parentElement.parentElement.parentElement.querySelector(".descricao");
-    const botoesComprar = elemento.target.parentElement.parentElement.parentElement.querySelector(".div-preco-produto2");
+    const botoesComprar = elemento.target.parentElement.parentElement.parentElement.querySelectorAll(".div-preco-produto2");
     const botoesComprar2 = elemento.target.parentElement.parentElement.parentElement.parentElement.querySelector(".div-preco-produto");
+    const botaoVoltar = elemento.target.parentElement.parentElement.parentElement.parentElement.querySelector(".botao-fechar");
     console.log(botoesComprar2)
     campoFixed.classList.toggle("displayFixo");
     descricaoParagrafoItem.classList.toggle("descricao-produto");
     botoesComprar.classList.toggle("displayON")
     botoesComprar2.classList.toggle("displayNone")
+    botaoVoltar.classList.toggle("displayON")
 
 })
     })
