c9a60b53669a356895e982ec6eeab4eff5aef875
Marcio Jose Martins Camara <118389170+marciomartins12@users.noreply.github.com>
2023-07-29 03:37:03 -0300

    Add files via upload
    botei um checkBOx input, porque n tava tava dando certo do jeito que tava fazendo.

--- app/exibirDescricao.js
+++ app/exibirDescricao.js
@@ -1,30 +1,19 @@
-exibirDescri()
-function exibirDescri() {
-    const prod = document.querySelectorAll(".item-clicavel");
-
-    prod.forEach((elemento) => {
-        
-        elemento.addEventListener("click", () => {
-         const exibirTudo = elemento.parentNode.parentNode;
-         const preco1 = elemento.parentElement.parentElement.parentElement.querySelector(".div-preco-produto");
-         const preco2 = elemento.parentElement.parentElement.querySelector(".div-preco-produto2");
-         const texto = elemento.parentElement.querySelector(".descricao-produto")
-         console.log(preco1)
-
-
-         exibirTudo.classList.add("displayFixo")
-         preco2.classList.remove("div-preco-produto2");
-         preco2.classList.add("div-descricao-produto3");
-         preco1.classList.add("displayNone")
-         texto.classList.add("displayON")
-})
-})
+function inputExibir(){
+    const inputs = document.querySelectorAll(".input");
     
+    inputs.forEach((elemento)=>{
+elemento.addEventListener("change", (elemento)=>{
+    const campoFixed = elemento.target.parentElement.parentElement.parentElement
+    const descricaoParagrafoItem = elemento.target.parentElement.parentElement.parentElement.querySelector(".descricao");
+    const botoesComprar = elemento.target.parentElement.parentElement.parentElement.querySelector(".div-preco-produto2");
+    const botoesComprar2 = elemento.target.parentElement.parentElement.parentElement.parentElement.querySelector(".div-preco-produto");
+    console.log(botoesComprar2)
+    campoFixed.classList.toggle("displayFixo");
+    descricaoParagrafoItem.classList.toggle("descricao-produto");
+    botoesComprar.classList.toggle("displayON")
+    botoesComprar2.classList.toggle("displayNone")
+
+})
+    })
 }
-function clickbotao(botaoVoltar, descricaoProduto, campoEsconderCarrinho, precoecarrinhoDisplayNone, containerFixo ){
-    botaoVoltar.addEventListener("click",()=>{
-        
-        })
-    
-}
-export default exibirDescri
+export default inputExibir
--- app/main.js
+++ app/main.js
@@ -1,9 +1,10 @@
 import produtos from "../pd.js";
-import exibirDescri from "./exibirDescricao.js";
+import inputExibir from "./exibirDescricao.js";
+
 
 import exibirItensNaTela from "./exibirProdutosNaTela.js";
 
 const lista = produtos;
 console.table(lista);
 exibirItensNaTela(lista);
-exibirDescri()
+inputExibir()
